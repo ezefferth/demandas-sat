655af701c6d12dcf00e39728e563e4e3f22ec928
ezefferth <ezefferth96@gmail.com>
2024-12-05 11:54:54 -0400

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Certifique-se de que o diretório de saída é o correto
+  },
   server: {
     host: true, // Permite acesso pela rede local
     port: 3000, // Defina a porta desejada
+    
     proxy: {
       '/api': {
         target: 'https://10.21.39.75:4001',
